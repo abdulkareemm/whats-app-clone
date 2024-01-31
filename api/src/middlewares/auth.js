7cd1c55bb53af56d92d7e0745be0ab2cc7b5bc21
abdulkareemm <eng.kreem.96@gmail.com>
2024-01-31 13:36:02 +0300

    auth middleware backend

---
+++ api/src/middlewares/auth.js
@@ -0,0 +1,15 @@
+import createHttpError from"http-errors"
+import  { verify } from"../utils/token"
+
+export const auth = async (req, res, next) => {
+  if (!req.headers["authorization"])
+    return next(createHttpError.Unauthorized());
+  const token = req.headers["authorization"].split(" ")[1];
+  const check = await verify(token, process.env.ACCESS_TOKEN_SECRET);
+  if (check) {
+    req.userId = check.userId;
+    next();
+  } else {
+    return next(createHttpError.Unauthorized());
+  }
+};
