0a9874cba94cffe3914e613b219b0cbc6d3381db
abdulkareemm <eng.kreem.96@gmail.com>
2024-01-31 07:48:09 +0300

    custom logger

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import app from "./src/apps/index.js";
+import  logger from"./src/configs/logger.js";
 
 
 
@@ -6,5 +7,23 @@ import app from "./src/apps/index.js";
 
 /// listening express
 let server = app.listen(process.env.PORT,()=>{
-    console.log("server is listening on port " + process.env.PORT)
+    logger.info("server is listening on port " + process.env.PORT)
 })
+
+///  handle server errors
+const exitHandler = ()=>{
+    if(server){
+        logger.info("Server Closed")
+        process.exit(1);
+
+    }else{
+        process.exit(1)
+    }
+}
+const unexpectedErrorHandler  = (error)=>{
+    logger.error(error)
+    exitHandler()
+}
+
+process.on("uncaughtException",unexpectedErrorHandler)
+process.on("unhandledRejection",unexpectedErrorHandler)
--- api/src/apps/index.js
+++ api/src/apps/index.js
@@ -1,17 +1,74 @@
-import express from 'express';
-import dotenv from "dotenv"
-
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+import createHttpError from "http-errors";
 
 /// dotenv configuration
-dotenv.config()
+dotenv.config();
 /// create express app
-
 const app = express();
 
+/// morgan
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 
+///  helmet
+app.use(helmet());
 
+///  parse json request url
+app.use(express.json());
 
+///  parse json request body
+app.use(express.urlencoded({ extended: true }));
 
+///  sanitize request data
+app.use(mongoSanitize());
+
+///  cookie parser
+app.use(cookieParser());
+
+/// gzip compression
+app.use(compression());
+
+///  file upload
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
+
+///  cors
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+///  routes
+//app.use("/api/v1", routers);
+
+///  page not fount
+app.use(async (req, res, next) => {
+  next(createHttpError.NotFound("This route does not exist."));
+});
+
+///  error handling
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    error: {
+      status: err.status || 500,
+      msg: err.message,
+    },
+  });
+});
 
 /// export express app
-export default app
+export default app;
